5bc8ad30f692ce87c886db164019d509fdd65c43
Anna Chan <anna@firma8.com>
2017-06-18 17:54:02 -0400

    Generics 2 Stack and StackIterator

--- playgrounds/Generics.playground/Contents.swift
+++ playgrounds/Generics.playground/Contents.swift
@@ -2,25 +2,99 @@
 
 import UIKit
 
-struct Stack {
-    var items = [Int]()
+struct StackIterator<T>: IteratorProtocol {
+    typealias Element = T
+    var stack: Stack<T>
+    mutating func next() -> Element? {
+        return stack.pop()
+    }
+}
+
+struct Stack<Element> {
+    var items = [Element]()
     
-    mutating func push(_ newItem: Int) {
+    mutating func push(_ newItem: Element) {
         items.append(newItem)
     }
     
-    mutating func pop() -> Int? {
+    mutating func pop() -> Element? {
         guard !items.isEmpty else {
             return nil
         }
         return items.removeLast()
     }
+    
+    func map<U>(_ f: (Element) -> U) -> Stack<U> {
+        var mappedItems = [U]()
+        for item in items {
+            mappedItems.append(f(item))
+        }
+        return Stack<U>(items: mappedItems)
+    }
 }
 
-var intStack = Stack()
+var intStack = Stack<Int>()
 intStack.push(1)
 intStack.push(2)
+var doubledStack = intStack.map { 2 * $0 }
 
 print(intStack.pop())
 print(intStack.pop())
 print(intStack.pop())
+
+print(doubledStack.pop())
+print(doubledStack.pop())
+
+var stringStack = Stack<String>()
+stringStack.push("this is a string")
+stringStack.push("another string")
+
+
+print(stringStack.pop())
+
+func myMap<T, U>(_ items: [T], _ f: (T) -> (U)) -> [U] {
+    var result = [U]()
+    for item in items {
+        result.append(f(item))
+    }
+    return result
+}
+
+let strings = ["one", "two", "three"]
+let stringLengths = myMap(strings) { $0.characters.count }
+print(stringLengths)
+
+func checkIfEqual<T: Equatable>(_ first: T, _ second: T) -> Bool {
+    return first == second
+}
+
+print(checkIfEqual(1, 1))
+print(checkIfEqual("a string", "a string"))
+print(checkIfEqual("a string", "a different string"))
+
+func checkIfDescriptionsMatch<T: CustomStringConvertible, U: CustomStringConvertible>(_ first: T, _ second: U) -> Bool {
+    return first.description == second.description
+}
+
+print(checkIfDescriptionsMatch(Int(1), UInt(1)))
+print(checkIfDescriptionsMatch(1, 1.0))
+print(checkIfDescriptionsMatch(Float(1.0), Double(1.0)))
+
+
+var myStack = Stack<Int>()
+myStack.push(10)
+myStack.push(20)
+myStack.push(30)
+
+var myStackIterator = StackIterator(stack: myStack)
+while let value = myStackIterator.next() {
+    print("got \(value)")
+}
+
+
+
+
+
+
+
+
