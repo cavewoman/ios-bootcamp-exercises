906be67f6b8de9cc12583d525db57f931ba5ec29
Anna Chan <anna@firma8.com>
2017-06-18 17:34:43 -0400

    Generics 1

---
+++ playgrounds/Generics.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Stack {
+    var items = [Int]()
+    
+    mutating func push(_ newItem: Int) {
+        items.append(newItem)
+    }
+    
+    mutating func pop() -> Int? {
+        guard !items.isEmpty else {
+            return nil
+        }
+        return items.removeLast()
+    }
+}
+
+var intStack = Stack()
+intStack.push(1)
+intStack.push(2)
+
+print(intStack.pop())
+print(intStack.pop())
+print(intStack.pop())
