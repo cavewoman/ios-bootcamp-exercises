f226590e41c361d5eeb14425e2ce8f1fe0c92771
Anna Chan <anna@firma8.com>
2017-06-18 14:16:39 -0400

    Protocols using model objects

--- playgrounds/Protocols.playground/Contents.swift
+++ playgrounds/Protocols.playground/Contents.swift
@@ -76,10 +76,27 @@ func printTable(_ data: [[String]], withColumnLabels columnLabels: String...) {
     }
 }
 
+struct Person {
+    let name: String
+    let age: Int
+    let yearsOfExperience: Int
+}
 
-let data = [
-    ["Joe", "30", "6"],
-    ["Karen", "40", "18"],
-    ["Fred", "50", "20"]
-]
-printTable(data, withColumnLabels: "Employee Name", "Age", "Years of Experience")
+struct Department {
+    let name: String
+    var people = [Person]()
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    mutating func add(_ person: Person) {
+        people.append(person)
+    }
+}
+
+var department = Department(name: "Engineering")
+department.add(Person(name: "Joe", age: 30, yearsOfExperience: 6))
+department.add(Person(name: "Karen", age: 40, yearsOfExperience: 18))
+department.add(Person(name: "Fred", age: 50, yearsOfExperience: 20))
+
