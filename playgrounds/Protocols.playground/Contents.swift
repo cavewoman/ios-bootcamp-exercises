81f508906bc401efa2156035da03f42761deebc7
Anna Chan <anna@firma8.com>
2017-06-18 14:38:13 -0400

    Protocols adding TabularDataSource Protocol

--- playgrounds/Protocols.playground/Contents.swift
+++ playgrounds/Protocols.playground/Contents.swift
@@ -52,21 +52,32 @@ class Product2: CustomStringConvertible {
 }
 
 // Examples from Book (Chapter 19 Protocols)
-func printTable(_ data: [[String]], withColumnLabels columnLabels: String...) {
+protocol TabularDataSource {
+    var numberOfRows: Int { get }
+    var numberOfColumns: Int { get }
+    
+    func label(forColumn column: Int) -> String
+    func itemFor(row: Int, column: Int) -> String
+}
+
+
+func printTable(_ dataSource: TabularDataSource) {
     var firstRow = "|"
     
     var columnWidths = [Int]()
     
-    for columnLabel in columnLabels {
+    for i in 0 ..< dataSource.numberOfColumns {
+        let columnLabel = dataSource.label(forColumn: i)
         let columnHeader = "\(columnLabel) |"
         firstRow += columnHeader
         columnWidths.append(columnLabel.characters.count)
     }
     print(firstRow)
     
-    for row in data {
+    for i in 0 ..<  dataSource.numberOfRows {
         var out = "|"
-        for (j, item) in row.enumerated() {
+        for j in 0 ..< dataSource.numberOfColumns {
+            let item = dataSource.itemFor(row: i, column: j)
             let paddingNeeded = columnWidths[j] - item.characters.count
             let padding = repeatElement(" ", count: paddingNeeded).joined(separator: "")
             out += "\(padding)\(item) |"
@@ -82,9 +93,13 @@ struct Person {
     let yearsOfExperience: Int
 }
 
-struct Department {
+struct Department: TabularDataSource, CustomStringConvertible {
     let name: String
     var people = [Person]()
+    
+    var description: String {
+        return "Department \(name)"
+    }
     
     init(name: String) {
         self.name = name
@@ -93,6 +108,33 @@ struct Department {
     mutating func add(_ person: Person) {
         people.append(person)
     }
+    
+    var numberOfRows: Int {
+        return people.count
+    }
+    
+    var numberOfColumns: Int {
+        return 3
+    }
+    
+    func label(forColumn column: Int) -> String {
+        switch column {
+        case 0: return "Employee Name"
+        case 1: return "Age"
+        case 2: return "Years of Experience"
+        default: fatalError("Invalid column!")
+        }
+    }
+    
+    func itemFor(row: Int, column: Int) -> String {
+        let person = people[row]
+        switch column {
+        case 0: return person.name
+        case 1: return String(person.age)
+        case 2: return String(person.yearsOfExperience)
+        default: fatalError("Invalid column!")
+        }
+    }
 }
 
 var department = Department(name: "Engineering")
@@ -100,3 +142,5 @@ department.add(Person(name: "Joe", age: 30, yearsOfExperience: 6))
 department.add(Person(name: "Karen", age: 40, yearsOfExperience: 18))
 department.add(Person(name: "Fred", age: 50, yearsOfExperience: 20))
 
+printTable(department)
+print(department)
