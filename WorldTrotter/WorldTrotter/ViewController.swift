bff3ae41478418b678fbfcad40d04824223e1617
Anna Chan <anna@firma8.com>
2017-06-19 11:44:38 -0400

    Adds world trotter app. Square in rectangle

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Anna Chan on 6/19/17.
+//  Copyright © 2017 Anna Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
+        
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.green
+        firstView.addSubview(secondView)
+    }
+
+}
+
