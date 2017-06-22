44c301c5601bb9956975dfa4dcdd9a28678d495f
Anna Chan <anna@firma8.com>
2017-06-22 09:53:47 -0400

    Simple touch app

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Anna Chan on 6/22/17.
+//  Copyright © 2017 Anna Sherman. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
