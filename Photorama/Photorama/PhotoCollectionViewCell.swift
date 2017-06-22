f5bd47151c622c8eeccfb3ea72ef96b7008a919f
Anna Chan <anna@firma8.com>
2017-06-22 17:26:54 -0400

    Adds collection view

---
+++ Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Anna Chan on 6/22/17.
+//  Copyright © 2017 Anna Sherman. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        update(with: nil)
+    }
+    
+    func update(with image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+}
+
